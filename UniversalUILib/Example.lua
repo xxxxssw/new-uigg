987e8d6da4f3cbd76a9382258cdaea49c3607fbd
xxxxssw <jiri.janda2011@gmail.com>
2026-07-04 13:44:33 +0200

    Update repository URL in Example.lua

--- UniversalUILib/Example.lua
+++ UniversalUILib/Example.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/xxxxssw/new-uigg/refs/heads/main/UniversalUILib/Library.lua"
+local repo = "https://raw.githubusercontent.com/xxxxssw/new-uigg/refs/heads/main/UniversalUILib"
 
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
