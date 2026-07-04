e293cae87fcbadf6c64d494891c502660f12123e
xxxxssw <jiri.janda2011@gmail.com>
2026-07-04 13:44:16 +0200

    Update Example.lua

--- UniversalUILib/Example.lua
+++ UniversalUILib/Example.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/OWNER/UniversalUILib/main/"
+local repo = "https://raw.githubusercontent.com/xxxxssw/new-uigg/refs/heads/main/UniversalUILib/Library.lua"
 
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
